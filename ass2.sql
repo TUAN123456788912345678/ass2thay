706784e2fa80169a63fa91468545a5d214ae0c92
tuan <vanminh291974@gmail.com>
2022-08-11 10:25:52 +0700

    first commit

---
+++ ass2.sql
@@ -0,0 +1,93 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Máy chủ: localhost
+-- Thời gian đã tạo: Th6 03, 2022 lúc 04:38 AM
+-- Phiên bản máy phục vụ: 10.4.21-MariaDB
+-- Phiên bản PHP: 8.1.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Cơ sở dữ liệu: `ass2`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `product`
+--
+
+CREATE TABLE `product` (
+  `product_id` int(11) NOT NULL,
+  `product_name` varchar(200) NOT NULL,
+  `product_price` varchar(100) NOT NULL,
+  `product_img` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Đang đổ dữ liệu cho bảng `product`
+--
+
+INSERT INTO `product` (`product_id`, `product_name`, `product_price`, `product_img`) VALUES
+(1, 'Optimus Prime', '$169.99', 'm.jpg'),
+(2, 'Hungarian Horntail Dragon', '$49.99', 'r.jpg'),
+(3, 'AT-AT™', '$799.99', 'a.jpg\r\n'),
+(4, 'LEGO® Titanic', '$629.99', 't.jpg'),
+(5, 'Imperial Star Destroyer™', '$699.99', 's.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `user`
+--
+
+CREATE TABLE `user` (
+  `user_id` int(11) NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Đang đổ dữ liệu cho bảng `user`
+--
+
+INSERT INTO `user` (`user_id`, `username`, `password`) VALUES
+(1, 'Quang', '123'),
+(2, 'Nhat', '121'),
+(3, 'Xuan', '111'),
+(4, 'Luong', '142'),
+(5, 'Ta', '134'),
+(6, 'Ngao', '153'),
+(7, 'Nang', '198'),
+(8, 'Ngo', '1898');
+
+--
+-- Chỉ mục cho các bảng đã đổ
+--
+
+--
+-- Chỉ mục cho bảng `product`
+--
+ALTER TABLE `product`
+  ADD PRIMARY KEY (`product_id`);
+
+--
+-- Chỉ mục cho bảng `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`user_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ detail.php
@@ -0,0 +1,88 @@
+﻿<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <title>Chi tiết bài hát</title>
+    <style type="text/css">
+        .images-detail img {
+        margin-top: 5%;
+        width: 100%;
+        align-items: center;
+        border-radius: 100%;
+        margin-bottom: 30px;
+        animation: app-logo-spin infinite 20s linear
+        }
+        @keyframes app-logo-spin {
+            from {
+                transform: rotate(0deg);
+            }
+            to {
+                transform: rotate(360deg);
+            }
+        }
+
+    </style>
+</head>
+<body>
+    <nav class="white">
+        </style>
+
+        <a href="index.php"><img src="https://pbs.twimg.com/media/EiOLaYbX0AA5S6S.jpg" width="100" height="100" /></a>
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" . data-target="#myNavbar">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+        </div>
+        <div class="collapse navbar-collapse" id="myNavbar">
+            <ul class="nav navbar-nav">
+                <li><a href="test1.php"><span class="glyphicon glyphicon-home"></span> Homepage</a></li>
+                <li><a href="">
+                        <sanp class="glyphicon glyphicon-earphone"></span>Contact
+                    </a></li>
+                <li><a href="login.php"><span class="glyphicon glyphicon-user"></span>Registration</a></li>
+                <li><a href="dangnhap.php"><span class="glyphicon glyphicon-log-in"></span> Log in</a></li>
+            </ul>
+            </ul>
+        </div>
+        </div>
+
+
+<div class="container">
+    <div class="row">
+        <?php
+        session_start();
+        $connect = mysqli_connect("3.132.234.157","tuan03","123@123a","tuan03");
+        $id = $_GET["id"];
+        $sql = "SELECT * FROM user,product WHERE product_id= {$id}";     
+        $result = mysqli_query($connect, $sql);
+        while($row= mysqli_fetch_array($result)){
+            $id = $row['Songid'];
+            ?>
+            <div class="col-md-6" style="text-align: left;">
+                <h2> Name of Music: <?php echo $row['product_name'];?> </h2>
+                <p>Price: <?php echo $row['product_price'];?> </p>
+             
+                            }
+                        }
+                       </script>
+            </div>  
+            <div class="images-detail">
+            <div class="col-md-6">
+                <img src="IMG/<?php echo $row[' product_img'] ?>" style = "width: 500px;height: 500px;">
+            </div> 
+        </div>
+            <?php
+        }
+
+    ?>
+    </div>
+</div>
+</body>
+</html>
